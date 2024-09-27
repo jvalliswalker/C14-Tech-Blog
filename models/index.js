3a368035ec7e09cd3754edf8761d529705d5be4b
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-26 18:49:21 -0700

    Create Comment model, import to index and establish relationships

--- models/index.js
+++ models/index.js
@@ -1,8 +1,17 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // Establish relationship between User and Post
-User.hasMany(Post);
 Post.belongsTo(User);
+User.hasMany(Post);
 
-module.exports = { User, Post }
+// Establish relationship between Comment and Post
+Comment.belongsTo(Post);
+Post.hasMany(Comment);
+
+// Establish relationship between Comment and User
+Comment.belongsTo(User);
+User.hasMany(Comment);
+
+module.exports = { User, Post, Comment }
