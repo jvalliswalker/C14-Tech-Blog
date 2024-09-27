f9906c8331f281f63f2b34251f111737cf234b42
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-26 18:40:05 -0700

    Create Post model, import to index and relate to User

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const User = require('./User');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [2]
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [2]
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id'
+      }
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  },
+  {
+    sequelize, // DB connection instance (from import) 
+    timestamps: false, 
+    freezeTableName: true, // Assures modelName is name of table in sql
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require('./User');
+const Post = require('./Post');
 
-// User
-// Posts
-// Comments
+// Establish relationship between User and Post
+User.hasMany(Post);
+Post.belongsTo(User);
 
-module.exports = { User }
+module.exports = { User, Post }
