65a3311a5e734d6b0bfd83981bf38cbc148c9a05
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-28 16:56:57 -0700

    Import and use api router

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const homeRoutes = require('./home-routes');
-
+const apiRoutes = require('./api/index');
 const router = require('express').Router();
 
+router.use('/api', apiRoutes)
 router.use('/', homeRoutes);
 
 module.exports = router;
