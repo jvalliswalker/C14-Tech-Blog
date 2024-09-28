d57b82f424bca265f1f1f08f620f631c2466a969
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-28 15:49:57 -0700

    Add dashboard placeholder code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../models/index');
+const { checkLoginStatus } = require('../utils/logging-handler');
 
 router.get('/', (req, res) => {
   res.render('homepage');
+})
+
+router.get('/dashboard', checkLoginStatus, async (req, res) => {
+  res.render('dashboard');
+})
+
+router.get('/login',(req, res) => {
+  res.render('login');
 })
 
 module.exports = router;
