985546af29684b32da9bea68f2421dce4e8f07c4
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-28 17:14:48 -0700

    Build out form data capture and ref to sign-in handler

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,40 @@
 // Variables
 // ====================
-
+const formIdSignIn = 'form-sign-in'; 
 
 // Process
 // ====================
 
 document.body.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  console.log('submitted');
-  getInputValues();
+  const formsData = getFormsData();
+  
+  if(Object.keys(formsData).includes(formIdSignIn)){
+    handleSignIn(formsData[formIdSignIn])
+  }
 })
 
 // Functions
 // ====================
 
-function getInputValues(){
+function getFormsData(){
 
+  // Query elements
   const inputs = document.querySelectorAll('input');
+  const forms = document.querySelectorAll('form');
+  
+  // Create and seed form from forms on page
+  const formIdToInputsMap = {};
 
-  for(const input of inputs){
-    console.log(input.value);
+  for(const form of forms){
+    formIdToInputsMap[form.id] = {};
   }
 
+  // Assign inputs to form map
+  for(const input of inputs){
+    formIdToInputsMap[input.dataset.formId][input.id] = input.value;
+  }
+
+  // Return map
+  return formIdToInputsMap;
 }
