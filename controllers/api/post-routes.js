622cbfae1f91a7f309ca6d9a1b8bbb66c4fe1081
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-18 19:39:55 -0700

    Update submit endpoint, add edit and delete endpoints

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,24 +1,77 @@
 const router = require("express").Router();
 const { Post } = require("../../models/index");
+const { withCatch } = require("../../utils/utils");
 
 router.post("/submit", async (req, res) => {
   try {
-    const newPost = Post.create({
-      title: req.body.title,
-      content: req.body.content,
-      user_id: req.session.user_id,
-    });
+    const newPost = Post.create(
+      {
+        title: req.body["new-post-title"],
+        content: req.body["new-post-content"],
+        user_id: req.session.user_id,
+      },
+      { individualHooks: true }
+    );
 
     if (newPost) {
-      res.status(200).json({
-        submit_successful: true,
-      });
+      res.status(200).send();
     } else {
-      res.status(400).json({
-        submit_successful: false,
-      });
+      res.status(400).send();
     }
   } catch (err) {
-    res.status(500).json({ submit_successful: false, error_data: err });
+    res.status(500).json(err);
   }
 });
+
+router.post("/edit", async (req, res) => {
+
+  console.log(req.body);
+
+  const { data, error } = await withCatch(
+    Post.update(
+      {
+        title: req.body["existing-post-title"],
+        content: req.body["existing-post-content"],
+      },
+      {
+        where: {
+          id: req.body.postId,
+        },
+      },
+      { individualHooks: true }
+    )
+  );
+
+  if (error) {
+    console.log(error.message);
+    res.status(500).json(error);
+  }
+
+  if (data) res.status(200).send();
+
+  else res.status(400).send();
+});
+
+router.delete("/delete/:postId", async (req, res) => {
+
+  const { data, error } = await withCatch(
+    Post.destroy(
+      {
+        where: {
+          id: req.params.postId,
+        },
+      }
+    )
+  );
+
+  if (error) {
+    console.log(error.message);
+    res.status(500).json(error);
+  }
+
+  if (data) res.status(200).send();
+
+  else res.status(400).send();
+});
+
+module.exports = router;
