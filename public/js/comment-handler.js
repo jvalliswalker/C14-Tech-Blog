7eed60a71e433032b63d227acbf67e98a5348483
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-18 21:28:11 -0700

    Add submit click listener & handler

--- public/js/comment-handler.js
+++ public/js/comment-handler.js
@@ -4,16 +4,41 @@ document.addEventListener("DOMContentLoaded", async function() {
 
     const comments = await getCommentsForPost();
 
+    // To do: 
+    // - turn console log into comment rendering over "placeholder text" in comment-post.handlebars (include commenter Username, date created, and comment content)
+    // - Test and confirm new post refreshes page and displays comments
+    // - Update posts displayed in homepage & dashboard to be minified version (title, date, and creator only)
+    // - Create "post" page (modify blog-post.handlebars), and include post's creators username
+    // Create idle logout functionality
+    // Record, ship
     console.log(comments);
 
-});
+    
+  });
 
+document.addEventListener('click', async (event) => {
+
+  if(event.target.id == "submit-post-comment"){
+    console.log('clicked');
+    
+    const submitResponse = await submitCommentToPost();
+
+    if(submitResponse.ok){
+      location.reload();
+    }
+    else{
+      console.log('submit comment error');
+    }
+  }
+})
+
+  
 async function getCommentsForPost(){
 
   const postCardElement = document.querySelector('div.card[data-post-id]');
   const postId = postCardElement.dataset.postId;
 
-  const response = await fetch(`/api/comment/:postId`, {
+  const response = await fetch(`/api/comment/${postId}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -34,18 +59,15 @@ async function submitCommentToPost(){
   const commentInput = document.querySelector('textarea');
   const postId = postCardElement.dataset.postId;
 
-  const response = await fetch(`/api/comment/:postId`, {
+  console.log('commentInput',commentInput);
+
+  const response = await fetch(`/api/comment/${postId}`, {
     method: 'POST',
-    body: JSON.stringify({ content: commentInput.innerHTML }),
+    body: JSON.stringify({ content: commentInput.value }),
     headers: {
       'Content-Type': 'application/json'
     }
   });
 
-  if(response.ok){
-    return await response.json();
-  }
-  else{
-    return [];
-  }
+  return response;
 }
