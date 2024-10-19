ffb692310c5f2375d83e18f024c3c14859aa504b
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-10-18 19:39:15 -0700

    Update home & dashboard routes, add edit post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,47 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models/index');
 const { checkLoginStatus } = require('../utils/logging-handler');
+const { withCatch } = require('../utils/utils');
 
 function isLoggedIn(req){
   return req.session ? req.session.logged_in : false;
 }
 
 router.get('/', async (req, res) => {
+  
+  const {data, error} = await withCatch(Post.findAll());
+  
+  if(error) res.status(500).send()
+
   res.render('homepage', {
-    loggedIn: isLoggedIn(req)
+    posts: data.map(x => x.get({plain: true}))
    } 
   );
 })
 
 router.get('/dashboard', checkLoginStatus, async (req, res) => {
-  res.render('dashboard', {
-    loggedIn: isLoggedIn(req)
-  });
+  
+  try {
+    
+    const usersPosts = await Post.findAll(
+      {
+        where: {
+          user_id: req.session.user_id
+        },
+        order: [
+          ['created_date', 'DESC']
+        ]
+      }
+    );
+
+    res.render('dashboard', {
+      loggedIn: isLoggedIn(req),
+      posts: usersPosts.map(x => x.get({plain: true}))
+    });
+  } catch (error) {
+    res.status(500);
+  }
+  
 })
 
 router.get('/new-post', checkLoginStatus, async (req, res) => {
@@ -40,6 +65,7 @@ router.get('/new-post', checkLoginStatus, async (req, res) => {
 
   res.render('new-post', {
     title: 'New Blog Post',
+    formId: 'form-new-post',
     fields: fields
   });
 })
@@ -51,5 +77,44 @@ router.get('/login',(req, res) => {
 router.get('/sign-up', async (req, res) => {
   res.render('sign-up')
 })
+
+router.get('/post/edit/:postId', checkLoginStatus, async (req, res) => {
+
+  const { data, error } = await withCatch(Post.findOne({
+    where: {
+      id: req.params.postId
+    }
+  }))
+
+  if (error) {
+    res.status(500).send();
+  }
+
+  if(!data){
+    res.status(400).send();
+  }
+
+  const fields = [
+    {
+      id: 'existing-post-title',
+      label: 'Title',
+      formId: 'form-edit-post',
+      type: 'text',
+      value: data.title
+    },
+    {
+      id: 'existing-post-content',
+      label: 'Content',
+      formId: 'form-edit-post',
+      type: 'textarea',
+      value: data.content,
+      isTextArea: true,
+      rows: 5
+    },
+  ]
+
+  res.render('edit-post', { fields: fields, title: 'Update Post', formId: 'form-edit-post', postId: req.params.postId });
+})
+
 
 module.exports = router;
